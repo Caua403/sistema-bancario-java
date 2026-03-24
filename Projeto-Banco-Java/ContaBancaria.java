087149e8e36fab9a09c96e7e9fc62908036b934b
Caua403 <cauapradomartins@outlook.com>
2026-03-24 15:57:04 -0300

    Add files via upload
    Primeiro Commit - Sistema Bancário java

---
+++ Projeto-Banco-Java/ContaBancaria.java
@@ -0,0 +1,60 @@
+package ContaBancaria;
+
+public class ContaBancaria {
+	private String nomeCliente;
+	private String numCliente;
+	private double saldo;
+	
+	
+	
+	
+	public ContaBancaria(String nomeCliente, String numCliente, double saldo) {
+		super();
+		this.nomeCliente = nomeCliente;
+		this.numCliente = numCliente;
+		this.saldo = saldo;
+	}
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public String getNumCliente() {
+		return numCliente;
+	}
+	public void setNumCliente(String numCliente) {
+		this.numCliente = numCliente;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public boolean sacar(double saque) {
+		if(saque <= saldo && saque > 0) {
+			saldo -= saque;
+			System.out.println("Saque de " + saque + " realizado. Saldo atual: " + saldo);
+			return true;
+		} else {
+			System.out.println("Saldo insuficiente.");
+			return false;
+			}
+		}
+	
+	public void depositar(double deposito) {
+		saldo += deposito;
+		System.out.println("Depósito de " + deposito + " reais realizado. Saldo atual: " + saldo);
+	}
+	
+	public String toString() {
+		String s = "";
+		 s += "Nome do cliente: " + nomeCliente + "; ";
+		 s += "Número do cliente " + numCliente + "; ";
+		 s += "Saldo: " + saldo;
+		return s;
+	}
+	
+}
